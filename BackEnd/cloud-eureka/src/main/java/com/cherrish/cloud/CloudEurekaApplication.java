de8d21ebae7d37339c732cd71ca96a6cf0b8a9b7
cherrishccl <528050239@qq.com>
2018-01-29 17:12:39 +0800

    added author

--- BackEnd/cloud-eureka/src/main/java/com/cherrish/cloud/CloudEurekaApplication.java
+++ BackEnd/cloud-eureka/src/main/java/com/cherrish/cloud/CloudEurekaApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author cherrish
+ * @time 2018-01-29 16:19
+ * @name CloudEurekaApplication
+ * @desc: registry center
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class CloudEurekaApplication {
